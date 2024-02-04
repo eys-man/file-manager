78c5159724becd1e095b2fdd9492bcbde7ed6182
eys-man <kapuec2010@gmail.com>
2024-02-04 23:46:47 +0300

    feat: add command line parsing; enter to home directory; add readline interface; handling application exit using ctrl-c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('hello!');
+import { EventEmitter } from 'events';
+import readline from 'readline';
+import os from 'os';
+import { chdir } from 'process';
+
+// разобрать командную строку (через argv) для определения имени пользователя
+let userName = '';
+if (process.argv.length <= 3 || process.argv[2] !== '--') userName = 'stranger';
+else userName = process.argv[3].slice(2).startsWith('username=') ?
+  process.argv[3].slice(11) : 'stranger';
+
+console.log(`Welcome to the File Manager, ${userName}`);
+
+process.chdir(os.homedir()); // перейти в папку пользователя
+console.log(`You are currently in '${process.cwd()}'`);
+
+const userInterface = readline.createInterface(process.stdin, process.stdout);
+
+userInterface
+  .on('line', (input) =>
+  {
+    console.log(`было введено '${input}'`);
+    // TODO: разбор введенного
+  })
+  .on('SIGINT', () => userInterface.close())
+  .on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0)
+  });
